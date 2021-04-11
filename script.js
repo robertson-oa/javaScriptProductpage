5a33bb695eb289964b88d6e1e551aa278f717273
Robertson otoo Asare <otoo.asare@amalitech.org>
2021-04-11 13:09:26 +0000

    navbar created & styled

---
+++ script.js
@@ -0,0 +1,20 @@
+let div = document.querySelector('div')
+
+div.insertAdjacentHTML('afterbegin',`<a href="#">Home</a>
+<a href="#">Sale</a>
+<a href="#">Kids</a>
+<a href="#">Boys</a>
+<a href="#">GIRLS</a>
+<a href="#">MORE SIZES</a>
+<a href="#">TIES</a>
+<a href="#">MEN</a>
+<a href="#">WOMEN</a>
+<a href="#">SALES</a>
+<a href="#">OUR STORY</a>`
+)
+
+div.classList.add("navbar");
+
+
+// const image = document.querySelector("image")
+// console
